af9f1d3fd1819a2eee30276e74116e3131440382
Mark Franklin <mark.franklin@mongodb.com>
2022-11-16 13:28:12 -0800

    WIP: updates mostly working

--- app-swift/App/Views/ItemDetail.swift
+++ app-swift/App/Views/ItemDetail.swift
@@ -5,41 +5,99 @@ import RealmSwift
 struct ItemDetail: View {
     // This property wrapper observes the Item object and
     // invalidates the view when the Item object changes.
-    @ObservedRealmObject var anOrder: order    
+    @ObservedRealmObject var anOrder: order
+    @State var isInEditMode: Bool?
+    @State var customerId = ""
+    @State var shipAddress = ""
+    @State var freight = 0.00
+    @State var orderDate = Date()
+    @State var shipCity = ""
+    @State var shipCountry = ""
+    @State var shipName = ""
+    @State var shipPostalCode = ""
     
     var body: some View {
         Form {
             Section(header: Text("Customer ID")) {
                 // Accessing the observed item object lets us update the live object
                 // No need to explicitly update the object in a write transaction
-                TextField("Customer ID", text: $anOrder.customerId)
-            }            
+                TextField("Customer ID", text: $customerId)
+            }
             Section(header: Text("Shipping Address")) {
-                TextField("Shipping Address", text: $anOrder.shipAddress)
+                TextField("Shipping Address", text: $shipAddress)
             }
             Section(header: Text("Freight")) {
-                TextField("Freight", value: $anOrder.freight, format: .number)
+                TextField("Freight", value: $freight, format: .number)
             }
             Section(header: Text("Order Date")) {
-                TextField("Order Date", value: $anOrder.orderDate, format: .dateTime)
+                TextField("Order Date", value: $orderDate, format: .dateTime)
             }
             Section(header: Text("City")) {
-                TextField("City", text: $anOrder.shipCity)
+                TextField("City", text: $shipCity)
             }
             Section(header: Text("Country")) {
-                TextField("Country", text: $anOrder.shipCountry)
+                TextField("Country", text: $shipCountry)
             }
             Section(header: Text("Name")) {
-                TextField("Name", text: $anOrder.shipName)
+                TextField("Name", text: $shipName)
             }
             Section(header: Text("Postal Code")) {
-                TextField("Postal Code", text: $anOrder.shipPostalCode)
+                TextField("Postal Code", text: $shipPostalCode)
             }
             NavigationLink(destination: OrderDetailsDetailsView(order: anOrder)) {
                 Text("Order Details")
             }
             
-        }
-        .navigationBarTitle("Update Order", displayMode: .inline)
+            Section {
+                            Button(action: {
+                                $anOrder.customerId.wrappedValue = customerId
+                                $anOrder.shipAddress.wrappedValue = shipAddress
+                                $anOrder.freight.wrappedValue = freight
+                                $anOrder.orderDate.wrappedValue = orderDate
+                                $anOrder.shipCity.wrappedValue = shipCity
+                                $anOrder.shipCountry.wrappedValue = shipCountry
+                                $anOrder.shipName.wrappedValue = shipName
+                                $anOrder.shipPostalCode.wrappedValue = shipPostalCode
+                                isInEditMode = false
+                                
+                            }) {HStack{
+                                Spacer()
+                                Text("Save")
+                                Spacer()
+                                
+                            }}
+                            Button(action: {
+                                // If the user cancels, we don't want to update the Realm object
+                                isInEditMode = true
+                                customerId = anOrder.customerId
+                                shipAddress = anOrder.shipAddress
+                                freight = anOrder.freight
+                                orderDate = anOrder.orderDate
+                                shipCity = anOrder.shipCity
+                                shipCountry = anOrder.shipCountry
+                                shipName = anOrder.shipName
+                                shipPostalCode = anOrder.shipPostalCode
+                            }) {
+                                HStack {
+                                    Spacer()
+                                    Text("Cancel")
+                                    Spacer()
+                                }
+                            }
+                        }
+            
+        }.onAppear(perform:{
+            isInEditMode = true
+            customerId = anOrder.customerId
+            shipAddress = anOrder.shipAddress
+            freight = anOrder.freight
+            orderDate = anOrder.orderDate
+            shipCity = anOrder.shipCity
+            shipCountry = anOrder.shipCountry
+            shipName = anOrder.shipName
+            shipPostalCode = anOrder.shipPostalCode
+            
+        })
+        .navigationBarTitle("Order: \(anOrder._id)", displayMode: .inline)
     }
 }
