9624da49a8c1f2520d229935265e6041f9a6c81b
Mark Franklin <mark.franklin@mongodb.com>
2022-11-17 12:49:23 -0800

    HACK: binding the realm object directly to the TextField

--- app-swift/App/Views/ItemDetail.swift
+++ app-swift/App/Views/ItemDetail.swift
@@ -16,33 +16,35 @@ struct ItemDetail: View {
     @State var shipName = ""
     @State var shipPostalCode = ""
     
+    //WARNING Don't bind a TextField directly to a realm object (anOrder in this case) like we are. This is only for demo purposes. 
+    
     var body: some View {
         Form {
             Section(header: Text("Customer ID")) {
                 // Accessing the observed item object lets us update the live object
                 // No need to explicitly update the object in a write transaction
-                TextField("Customer ID", text: $customerId)
+                TextField("Customer ID", text: $anOrder.customerId)
             }
             Section(header: Text("Shipping Address")) {
-                TextField("Shipping Address", text: $shipAddress)
+                TextField("Shipping Address", text: $anOrder.shipAddress)
             }
             Section(header: Text("Freight")) {
-                TextField("Freight", value: $freight, format: .number)
+                TextField("Freight", value: $anOrder.freight, format: .number)
             }
             Section(header: Text("Order Date")) {
-                TextField("Order Date", value: $orderDate, format: .dateTime)
+                TextField("Order Date", value: $anOrder.orderDate, format: .dateTime)
             }
             Section(header: Text("City")) {
-                TextField("City", text: $shipCity)
+                TextField("City", text: $anOrder.shipCity)
             }
             Section(header: Text("Country")) {
-                TextField("Country", text: $shipCountry)
+                TextField("Country", text: $anOrder.shipCountry)
             }
             Section(header: Text("Name")) {
-                TextField("Name", text: $shipName)
+                TextField("Name", text: $anOrder.shipName)
             }
             Section(header: Text("Postal Code")) {
-                TextField("Postal Code", text: $shipPostalCode)
+                TextField("Postal Code", text: $anOrder.shipPostalCode)
             }
             NavigationLink(destination: OrderDetailsDetailsView(order: anOrder)) {
                 Text("Order Details")
