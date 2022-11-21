af528842512797acb348185b0dac9e9e18fced74
Sig Narváez <sigfrido@mongodb.com>
2022-11-21 15:58:39 -0800

    int2 (smallint) to int8 (bigint)

---
+++ atlas/clean-data.js
@@ -0,0 +1,12 @@
+db.categories.remove({});
+db.customers.remove({});
+db.employees.remove({});
+db.employeeTerritories.remove({});
+db.Item.remove({});
+db.orders.remove({});
+db.products.remove({});
+db.region.remove({});
+db.shippers.remove({});
+db.suppliers.remove({});
+db.territories.remove({});
+db.usStates.remove({});
